c159dda0aa83e9ab51b4f56bf74cab7e0a5f82ad
Sean Jacobson <sean.jacobson86@gmail.com>
2020-01-14 20:27:28 -0700

    Scafolded Function
    Scafolded the function for #add-to-do so that I can get an idea of how everything is supposed to look.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,22 @@
+// Global variables
+let listCount = 1;
+
 // function for the #add-to-do button
 $("#add-to-do").on("click", function(event) {
     event.preventDefault();
     console.log("Clicked button");
+
+    // Get the value of #to-do
+    // Set $("<p>") to a variable
+    // Give p the text to-do
+    // Set the id attr of p to Item-listCount
+    // Set $("<button>") to a variable
+    // Give the button an id of listCount
+    // Give the button an attr of data-to-do listCount
+    // Give the button a class of checkbox
+    // Give the button a text of X
+    // Prepend the button to p
+    // Add everything to the #toDoList
+    // Add to the listCount
+    // Clear the #to-do text box
 })
