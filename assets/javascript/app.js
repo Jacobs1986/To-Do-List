75778c57b8878daffee268def762299fef8bb0da
Sean Jacobson <sean.jacobson86@gmail.com>
2020-01-16 18:34:00 -0700

    Created generate and remove
    I wrote the script for generating the styling needed when genrating the list from localstorage and also the code for removing items from the array and saving it to the storage.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,37 @@
 
 // Global variables
 let listCount = 1;
-let toDoArray = [];
+let toDoArray = JSON.parse(localStorage.getItem('list'));
+
+// get the list from local storage
+console.log(toDoArray);
+
+// generate styling for the list
+toDoGenerate = toDoItem => {
+     // Set $("<p>") to a variable
+     let paragraph = $("<p>");
+     // Give p the text to-do
+     paragraph.text(toDoItem);
+     // Set the id attr of p to Item-listCount
+    paragraph.attr("id", `Item-${listCount}`);
+    // Set $("<button>") to a variable
+    let button = $("<button>");
+    // Give the button an id of listCount
+    button.attr("id", `${listCount}`);
+    // Give the button an attr of data-to-do listCount
+    button.attr("data-to-do", `${listCount}`);
+    // Give the button a class of checkbox
+    button.addClass("checkbox");
+    // Give the button a text of X
+    button.text("x");
+    // Prepend the button to p
+    paragraph.prepend(button);
+    // Add everything to the #toDoList
+    $("#toDoList").append(paragraph);
+    // Add to the listCount
+    listCount++
+}
+toDoArray.forEach(toDoGenerate);
 
 // function for the #add-to-do button
 $("#add-to-do").on("click", function(event) {
@@ -13,7 +43,7 @@ $("#add-to-do").on("click", function(event) {
     let toDoItem = $("#to-do").val().trim();
     console.log(toDoItem);
     // Set $("<p>") to a variable
-    paragraph = $("<p>");
+    let paragraph = $("<p>");
     // Give p the text to-do
     paragraph.text(toDoItem);
     console.log(paragraph);
@@ -21,7 +51,7 @@ $("#add-to-do").on("click", function(event) {
     paragraph.attr("id", `Item-${listCount}`);
     console.log(paragraph);
     // Set $("<button>") to a variable
-    button = $("<button>");
+    let button = $("<button>");
     // Give the button an id of listCount
     button.attr("id", `${listCount}`);
     // Give the button an attr of data-to-do listCount
@@ -51,6 +81,11 @@ $(document.body).on("click", ".checkbox", function () {
     // get the number from the data-to-do attr store it in a variable
     let toDoNumber = $(this).attr("data-to-do");
     console.log(toDoNumber);
+    // Remove from the array
+    toDoArray.splice((toDoNumber - 1), 1);
+    console.log(toDoArray)
+    // save to localStorage
+    localStorage.setItem('list', JSON.stringify(toDoArray));
     // Empty the <p> element using the #Item-variable id
     $(`#Item-${toDoNumber}`).remove();
 })
